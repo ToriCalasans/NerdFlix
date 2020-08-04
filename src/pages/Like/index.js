04bf9a150d9be4651afc956b8222d4aa6c65bffc
ToriCalasans <calasanstori@gmail.com>
2020-08-04 18:52:46 -0300

    Arrumei o form, a categoria e o like

--- src/pages/Like/index.js
+++ src/pages/Like/index.js
@@ -3,9 +3,32 @@ import PageDefault from '../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function LikePagina() {
+
+
     return (
         <PageDefault>
-            <h1> Obrigada por curtir nossa página.</h1>
+        <div>
+        <h1> Obrigada por curtir nossa página.</h1>
+        <progress value="50" max="100" > Progresso em 50 %</progress>
+        <h1>Nosso site ainda está em desenvolvimento mas em breve vai funcionar lindamente.</h1>
+
+        
+            <label> Avalie nosso trabalho
+                <input
+                    type = "range"
+                    name = "nota"                
+                    onChange = "onChange"
+                    list = "tickmarks"
+                />
+            </label>
+            
+        </div>
+
+        
+            
+            <form>
+
+            </form>
             <Link to='/'>Voltar para home</Link>
         </PageDefault>
     )

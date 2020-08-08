daaa37bda8add698f247bc96b653d6d177432590
ToriCalasans <calasanstori@gmail.com>
2020-08-08 02:30:05 -0300

    mexi no 404, tem q por o joguinho dps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable react/jsx-equals-spacing */
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
 import Home from './pages/Home';
@@ -9,17 +10,19 @@ import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 import LikePagina from './pages/Like';
 
+const Pagina404 = () => (
+    <div>
+        erro 404
+    </div>
+);
 
-const Pagina404 = () => (<div> Página 404 </div>)
-
-ReactDOM.render( 
+ReactDOM.render(
     <BrowserRouter>
         <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/cadastro/video" component= {CadastroVideo} />
             <Route path="/cadastro/categoria" component= {CadastroCategoria} />
             <Route path="/like" component= {LikePagina} />
-            
             <Route component={Pagina404} />
         </Switch>
     </BrowserRouter>,
